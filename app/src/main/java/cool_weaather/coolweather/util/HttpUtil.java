1595aaee9331a086d88bf01e3d4f578e384e1c9c
LZ <1076765928@qq.com>
2018-06-07 18:30:19 +0800

    fsefsef

---
+++ app/src/main/java/cool_weaather/coolweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package cool_weaather.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by 10767 on 2018/6/5.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
